1ecc558e638e376c4dd6510a6b3fe06a9229b20c
jorgemhtdev <jorge@jorgemht.dev>
2021-01-28 10:30:53 +0100

    [Added] - Project

---
+++ EmbeddedFontsFontIcons/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("PottaOne-Regular.ttf", Alias = "PottaOne")]
+[assembly: ExportFont("fa-brands-400.ttf", Alias = "IconFontBrands")]
+[assembly: ExportFont("fa-regular-400.ttf", Alias = "IconFontRegular")]
+[assembly: ExportFont("fa-solid-900.ttf", Alias = "IconFontSolid")]
